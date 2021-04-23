3cfc23948895feb97f2c95dde5dbe6e0955f28d9
Tyler <tjpegram@vt.edu>
2021-04-23 03:25:35 -0400

    Initial Commit

---
+++ image_preprocessing.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot
+
+# File that will contain image preprocessing code for before feature detection
+
+
+def histogram_equalize():
+    pass
---
+++ main.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import skimage
+import image_preprocessing as pre
+
+
+# Main file which will contain a driver that will load the images and run the respective algorithm.
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()

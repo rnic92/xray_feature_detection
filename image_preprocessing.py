3e649c27b69cf82373834399789905b1ac8daa85
Tyler <tjpegram@vt.edu>
2021-04-27 16:05:45 -0400

    Added a few pre processing functions

--- image_preprocessing.py
+++ image_preprocessing.py
@@ -1,9 +1,30 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot
-
+from skimage import exposure, feature
+from scipy import ndimage
 # File that will contain image preprocessing code for before feature detection
 
 
-def histogram_equalize():
-    pass
+def histogram_equalize(img):
+    # Equalize the given image's histogram
+    eq_image = exposure.equalize_hist(img)
+    return eq_image
+
+
+def gaussian_filter(img, sigma):
+    # Apply a gaussian filter to the given image with the given sigma
+    filtered_image = ndimage.gaussian_filter(img, sigma)
+    return filtered_image
+
+
+def canny_det(img, sigma):
+    # Apply a Canny Edge Detector to the given image with the given sigma
+    edges = feature.canny(img, sigma)
+    return edges
+
+
+def binary_threshold(img, thresh):
+    # Apply a binary threshold of the given value to the given image
+    ret, threshold_image = cv2.threshold(img, thresh, 1, cv2.THRESH_BINARY)
+    return threshold_image
--- main.py
+++ main.py
@@ -9,7 +9,25 @@ import image_preprocessing as pre
 
 
 def main():
-    pass
+    image = plt.imread("person100_bacteria_475.jpeg")
+    plt.imshow(image, cmap="gray")
+    plt.show()
+    eq_image = pre.histogram_equalize(image)
+    plt.imshow(eq_image, cmap="gray")
+    plt.show()
+    gauss_filtered = pre.gaussian_filter(eq_image, 3)
+    plt.imshow(gauss_filtered, cmap="gray")
+    plt.show()
+    threshed = pre.binary_threshold(eq_image, .5)
+    plt.imshow(threshed, cmap="gray")
+    plt.show()
+    edges = pre.canny_det(threshed, 5)
+    plt.imshow(edges, cmap="gray")
+    plt.show()
+    selected = edges+eq_image  # Just for fun to see what it looks like
+    selected = pre.histogram_equalize(selected)
+    plt.imshow(selected, cmap="gray")
+    plt.show()
 
 
 if __name__ == "__main__":

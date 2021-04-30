6da696f8e4d4826ef347eacff06ffbe188ccff32
rnic92 <rnic92@vt.edu>
2021-04-30 16:21:58 -0400

    quad and sixteens

--- image_preprocessing.py
+++ image_preprocessing.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot
 from skimage import exposure, feature
 from scipy import ndimage
+from PIL import Image, ImageOps
 # File that will contain image preprocessing code for before feature detection
 
 
@@ -47,3 +48,31 @@ def sobel_filters(img):
     grad = grad/grad.max() * 255
     angle = np.arctan2(sobel_vertical, sobel_horizontal)
     return [grad.astype(np.uint8), angle.astype(np.uint8)]
+
+
+def quadImage(img):
+    width,height = img.size
+    q1 = img.crop((0,0,width/2,height/2))
+    q2 = img.crop((width/2,0,width,height/2))
+    q3 = img.crop((0,height/2,width/2,height))
+    q4 = img.crop((width/2,height/2,width,height))
+    return q1,q2,q3,q4
+
+
+def sixteenImage(img):
+    """
+    takes in an image and returns list of 16 sections
+    1  2  ||  5  6
+    3  4  ||  7  8
+    ===============
+    9 10  ||  13 14
+    11 12 ||  15 16
+    """
+    width,height = img.size
+    q1,q2,q3,q4 = quadImage(img)
+    s1,s2,s3,s4 = quadImage(q1)
+    s5,s6,s7,s8 = quadImage(q2)
+    s9,s10,s11,s12 = quadImage(q3)
+    s13,s14,s15,s16 = quadImage(q4)
+    ret = [s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16]
+    return ret
--- nicmain.py
+++ nicmain.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import skimage
 import image_preprocessing as pre
 import os
-from PIL import Image
+from PIL import Image, ImageOps
 
 def main():
     #read in all files
@@ -16,17 +16,47 @@ def main():
     filename = "person3_virus_15.jpeg"
     img = Image.open(path+filename)
     img = img.resize((width, height))
+    q1,q2,q3,q4 = quadImage(img)
+    q1arry = np.array(q1)
+    print(q1arry)
+
+    sixteen = sixteenimage(img)
+    histeq = ImageOps.equalize(q1)
+    histeq.show()
+    grad1, angle = pre.sobel_filters(histeq)
+    plt.imshow(grad1)
+    plt.show()
+    plt.imshow(angle)
+    plt.show()
 
 
-
-
-def quadimage(img):
+def quadImage(img):
+    width,height = img.size
     q1 = img.crop((0,0,width/2,height/2))
     q2 = img.crop((width/2,0,width,height/2))
     q3 = img.crop((0,height/2,width/2,height))
     q4 = img.crop((width/2,height/2,width,height))
-
     return q1,q2,q3,q4
+
+
+def sixteenImage(img):
+    """
+    takes in an image and returns list of 16 sections
+    1  2  ||  5  6
+    3  4  ||  7  8
+    ===============
+    9 10  ||  13 14
+    11 12 ||  15 16
+    """
+    width,height = img.size
+    q1,q2,q3,q4 = quadImage(img)
+    s1,s2,s3,s4 = quadImage(q1)
+    s5,s6,s7,s8 = quadImage(q2)
+    s9,s10,s11,s12 = quadImage(q3)
+    s13,s14,s15,s16 = quadImage(q4)
+    ret = [s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16]
+    return ret
+
 
 def readall(path):
     for filename in os.listdir(path):
